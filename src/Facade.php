79cba9d69717b42276c17498e152851297d5fe36
C.Jason <chenjxlg@163.com>
2020-06-28 12:00:49 +0800

    doc

--- src/Facade.php
+++ src/Facade.php
@@ -4,6 +4,12 @@ namespace Jason\Sms;
 
 use Illuminate\Support\Facades\Facade as LaravelFacade;
 
+/**
+ * Class Facade
+ * @package Jason\Sms
+ * @method static \Jason\Sms\Sms send($mobile, $channel)
+ * @method static \Jason\Sms\Sms check($mobile, $code, $channel)
+ */
 class Facade extends LaravelFacade
 {
     protected static function getFacadeAccessor()
