c7e21233627835aa35541fb5d3fd5b4e7f89da6c
C.Jason <chenjxlg@163.com>
2020-07-09 09:55:23 +0800

    支持多字段模板

--- src/Sms.php
+++ src/Sms.php
@@ -13,14 +13,14 @@ class Sms
     /**
      * Notes: 发送短信
      * @Author: <C.Jason>
-     * @Date: 2020/1/14 4:34 下午
-     * @param string $mobile 手机号码
+     * @Date  : 2020/1/14 4:34 下午
+     * @param string $mobile  手机号码
      * @param string $channel 验证通道
-     * @param array $gateways 发送网关
+     * @param array  $appends 验证码附加内容
      * @return bool
      * @throws SmsSendException
      */
-    public function send(string $mobile, string $channel = 'DEFAULT', array $gateways = [])
+    public function send(string $mobile, string $channel = 'DEFAULT', array $appends = [])
     {
         try {
             $config = config('sms');
@@ -29,27 +29,30 @@ class Sms
                 throw new SmsSendException('不合法的验证通道' . $config['template'][$channel]);
             }
 
-            DB::transaction(function () use ($mobile, $channel, $config) {
+            DB::transaction(function () use ($mobile, $channel, $config, $appends) {
+                $data = [];
                 if ($config['debug']) {
-                    $code = $config['debug_code'];
+                    $data['code'] = $config['debug_code'];
                 } else {
-                    $code = sprintf("%0" . $config['length'] . "d", mt_rand(1, pow(10, $config['length']) - 1));
+                    $data['code'] = sprintf("%0" . $config['length'] . "d", mt_rand(1, pow(10, $config['length']) - 1));
+                }
+
+                if (!empty($appends)) {
+                    $data = array_merge($data, $appends);
                 }
 
                 if ($config['debug'] != true) {
                     $easySms = new EasySms($config);
                     $easySms->send($mobile, [
                         'template' => $config['template'][$channel],
-                        'data'     => [
-                            'code' => $code,
-                        ],
+                        'data'     => $data,
                     ]);
                 }
 
                 SmsModel::create([
                     'mobile'  => $mobile,
                     'channel' => $channel,
-                    'code'    => $code,
+                    'code'    => $data['code'],
                 ]);
             });
 
@@ -62,10 +65,10 @@ class Sms
     /**
      * 验证短信
      * @Author:<C.Jason>
-     * @Date:2018-11-07T14:26:38+0800
-     * @param string $mobile [description]
-     * @param string $code [description]
-     * @param string $channel [description]
+     * @Date  :2018-11-07T14:26:38+0800
+     * @param string $mobile  手机号码
+     * @param string $code    验证码
+     * @param string $channel 验证通道
      * @return bool
      */
     public function check(string $mobile, string $code, string $channel = 'DEFAULT')
